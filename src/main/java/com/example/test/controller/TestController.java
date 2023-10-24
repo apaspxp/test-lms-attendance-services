4122699f65d3081e57c88be2338c7170e7b5b080
rajesh1420621 <panigrahirajesh578@gmail.com>
2023-10-24 21:14:55 +0530

    jenkinsfile changed

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.example.test.controller;
 
+import com.example.test.entity.AttendanceEntity;
+import com.example.test.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+
+    @Autowired
+    private TestService testService;
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String test(){
         return "Test Sucsess";
+    }
+
+    @RequestMapping(value = "findall", method = RequestMethod.GET)
+    public List<AttendanceEntity> findAll() {
+        return testService.findAll();
     }
 }
--- src/main/java/com/example/test/service/TestService.java
+++ src/main/java/com/example/test/service/TestService.java
@@ -1,4 +1,19 @@
 package com.example.test.service;
 
+import com.example.test.entity.AttendanceEntity;
+import com.example.test.repo.AttendanceRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TestService {
+
+    @Autowired
+    private AttendanceRepo attendanceRepo;
+
+    public List<AttendanceEntity> findAll(){
+        return attendanceRepo.findAll();
+    }
 }
